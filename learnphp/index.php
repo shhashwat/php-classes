cc8c2bc6613b3395d4aca0e43e9a90245f3757ca
Shashwat Singh <sshashwatssingh@gmail.com>
2025-03-26 18:41:14 +0530

    meh

--- learnphp/index.php
+++ learnphp/index.php
@@ -1,13 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="index.php" method="post">
+        <label for="x">x:</label>
+        <input type="text" name="x"/><br>
+        <label for="y">y:</label>
+        <input type="text" name="y"/><br>
+        <label for="z">z:</label>
+        <input type="text" name="z"/><br>
+        <input type="submit" value="total">
+    </form>
+</body>
+</html>
+
 <?php
-    $name = "Yash";
-    $quanity = 5;
-    $price = 7.99;
-    $food = "pizza";
+    $x = $_POST["x"];
+    $y = $_POST["y"];
+    $z = $_POST["z"];
     $total = null;
 
-    echo "You have ordered $quanity $food(s) for $name at a price of \$$price per $food <br>";
+    // $total = abs($x);
+    // $total = round($x);
+    // $total = floor($x);
+    // $total = ceil($x);
+    // $total = pow($x,$y);
+    // $total = sqrt($x);
+    // $total = max($x,$y,$z);
+    // $total = min($x,$y,$z);
+    // $total = pi();
+    $total = rand(1,100);
 
-    $total = $quanity * $price;
-    echo "Your total is \$$total <br>";
-    echo"You can pay at the front register";
+    echo $total;
 ?>
