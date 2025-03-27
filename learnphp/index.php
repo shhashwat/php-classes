6311c70702bd225995ac3f2303192d8612ac88be
Shashwat Singh <sshashwatssingh@gmail.com>
2025-03-27 12:26:43 +0530

    logical operatos

--- learnphp/index.php
+++ learnphp/index.php
@@ -1,13 +1,29 @@
 <?php
-    $child = false;
-    $senior = false;
-    $ticket = null;
-
-    if($child || $senior) {
-        $ticket = 10;
-    } else {
-        $ticket = 20;
+    $date = date("l");
+    
+    switch ($date) {
+        case "Monday":
+            echo"$date Work day";
+            break;
+        case "Tuesday":
+            echo"$date Work day";
+            break;
+        case "Wednesday":
+            echo"$date Work day";
+            break;
+        case "Thursday":
+            echo"$date Work day";
+            break;
+        case "Friday":
+            echo"$date Work day";
+            break;
+        case "Saturday":
+            echo"$date Weekend";
+            break;
+        case "Sunday":
+            echo"$date Weekend";
+            break;
+        default:
+            echo"$date Error";
     }
-
-    echo"The ticket price is $ticket";
 ?>
