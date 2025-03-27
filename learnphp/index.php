65d38136b87530958477414593c1670019a8760c
Shashwat Singh <sshashwatssingh@gmail.com>
2025-03-27 18:39:40 +0530

    str funcs

--- learnphp/index.php
+++ learnphp/index.php
@@ -1,10 +1,19 @@
 <?php
+    $username = "Ward Corpse";
+    $phone_number = "456-567-7890";
+    // $username = strtolower($username);
+    // $username = strtoupper($username);
+    // $username = trim($username);
+    // $username = str_pad($username,20,"0");
+    // $phone_number = str_replace("-", "/", $phone_number);
+    // $email = "jTf0s@example.com";
+    // $email = strrev($email);
+    // $email = str_shuffle($email);
 
-    function hypotenuse(int $a, int $b) {
-        $c = pow($a, 2) + pow($b, 2);
-        return sqrt($c);
+    $fullname = "Shashwat Singh Raghuvanshi";
+    $fullname = explode("-", $fullname);
+
+    foreach ($fullname as $name){
+        echo"$name <br>";
     }
-
-    echo hypotenuse(3,4);
-
 ?>
