859c3277550a1a6506ae575ec3ff89f1722519dc
Shashwat Singh <sshashwatssingh@gmail.com>
2025-03-27 12:15:33 +0530

    logical operators

--- learnphp/index.php
+++ learnphp/index.php
@@ -1,20 +1,13 @@
 <?php
-    $temp = 99;
-    $cloudy = true;
-    
-    if($temp >= 10 && $temp <= 32 ){
-        echo "$temp<sup>o</sup>C is nice outside.";
-    } elseif($temp < 10){
-        echo "$temp<sup>o</sup>C is feezing my balls.";
-    } elseif($temp > 100){
-        echo "Gunna die lol";
-    } else{
-        echo "Its hot as FUCK";
+    $child = false;
+    $senior = false;
+    $ticket = null;
+
+    if($child || $senior) {
+        $ticket = 10;
+    } else {
+        $ticket = 20;
     }
-    echo"<br>";
-    if(!$cloudy){
-        echo "Least its not cloudy";
-    } else{
-        echo "Fuck this";
-    }
+
+    echo"The ticket price is $ticket";
 ?>
