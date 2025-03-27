91fd1d4342dae979ab42084da80dd999fa82480d
Shashwat Singh <sshashwatssingh@gmail.com>
2025-03-27 11:35:26 +0530

    hourly rate

--- learnphp/index.php
+++ learnphp/index.php
@@ -1,35 +1,22 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <form action="index.php" method="post">
-        <label for="age">Enter your age:</label>
-        <input type="text" name="age">
-        <input type="submit" value="Submit">
-    </form>
-</body>
-</html>
-
 <?php
-    // $age = $_POST["age"];
+    $hours = 56;
+    $rate = 15;
+    $weekly_pay = null;
+    $isValid = true;
 
-    // if ($age >= 18 && $age <= 99) {
-    //     echo "You are old enough to vote!";
-    // } else if ( $age >= 100) {
-    //     echo "You are too old, rest at home and don't vote.";
-    // } else {
-    //     echo "You are not old enough to vote.";
-    // };
-
-    $adult = true;
-
-    if($adult){
-        echo "You are old enough to vote!";
+    if($hours < 0) {
+        $isValid = false;
+    } elseif($hours <= 40) {
+        $weekly_pay = $hours * $rate;
     } else {
-        echo "You are not old enough to vote.";
+        $overtime = $hours - 40;
+        $overtime_rate = 20;
+        $weekly_pay = (40 * $rate) + ($overtime * $overtime_rate);
+    };
+
+    if($isValid) {
+        echo "You made \$$weekly_pay this week";
+    } else {
+        echo "Hours must be greater than 0";
     };
 ?>
