82ff768c4c764d2c9890f9a57edc3aa773bd87b6
Shashwat Singh <sshashwatssingh@gmail.com>
2025-03-27 18:26:10 +0530

    functions

--- learnphp/index.php
+++ learnphp/index.php
@@ -1,26 +1,10 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <form action="index.php" method="post">
-        <input type="checkbox" name="foods[]" value="Pizza"> Pizza <br>
-        <input type="checkbox" name="foods[]" value="Burger"> Burger <br>
-        <input type="checkbox" name="foods[]" value="Fries"> Fries <br>
-        <input type="checkbox" name="foods[]" value="Drinks"> Drinks <br>
-        <input type="submit" name="check">
-    </form>
-</body>
-</html>
-
 <?php
-    if(isset($_POST["check"])){
-        $foods = $_POST["foods"];
-        foreach($foods as $food) {
-            echo $food . "<br>";
-        }
+
+    function hypotenuse(int $a, int $b) {
+        $c = pow($a, 2) + pow($b, 2);
+        return sqrt($c);
     }
+
+    echo hypotenuse(3,4);
+
 ?>
