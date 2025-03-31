7067982b30fcebd1771acd0076207d8b8e3486ab
Shashwat Singh <sshashwatssingh@gmail.com>
2025-03-31 15:40:58 +0530

    Welcome to INDIEPOP - done with php

--- learnphp/index.php
+++ learnphp/index.php
@@ -1,20 +1,48 @@
 <?php
     include("database.php");
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="<?php htmlspecialchars($_SERVER["PHP_SELF"])?>" method="post">
+        <h2>Welcome to IndiePop!</h2>
+        username: <br>
+        <input type="text" name="username"><br>
+        password: <br>
+        <input type="password" name="password"><br>
+        <input type="submit" value="register" name="submit">
+    </form>
+</body>
+</html>
 
-    $sql = "SELECT * FROM users";
+<?php
+    if($_SERVER['REQUEST_METHOD'] == "POST"){
+        $username = filter_input(INPUT_POST,"username", FILTER_SANITIZE_SPECIAL_CHARS);
+        $password = filter_input(INPUT_POST,"password", FILTER_SANITIZE_SPECIAL_CHARS);
 
-    $result = mysqli_query($conn, $sql);
+        if(empty($username)){
+            echo "Please enter a username";
+        } elseif (empty($password)){
+            echo "Please enter a password";
+        } else {
+            $hash = password_hash($password, PASSWORD_DEFAULT);
+            $sql = "INSERT INTO users (user, password) VALUES ('$username', '$hash')";
 
-    if(mysqli_num_rows($result) > 0){
-        while($row = mysqli_fetch_assoc($result)){
-            echo "ID: " . $row["id"] . "<br>";
-            echo "User: ". $row["user"] ."<br>";
-            echo "Registered: ". $row["reg_date"] ."<br>";
-            echo "<br>";
+            try {
+                //code...
+                mysqli_query($conn, $sql);
+                echo "Successfully registered!";
+            } catch (mysqli_sql_exception) {
+                //throw $th;
+                echo "The username already exists";
+            }
         }
-    } else{
-        echo "0 results";
-    }
+    };
 
     mysqli_close($conn);
 ?>
