7485b5557658366f443d47d9bd2a3681ccbabc12
Shashwat Singh <sshashwatssingh@gmail.com>
2025-03-31 12:26:04 +0530

    cookies

--- learnphp/index.php
+++ learnphp/index.php
@@ -1,19 +1,11 @@
 <?php
-    include("headers.html");
-?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    This is the home page <br>
-    Stuff about your homepage can go here <br>
-</body>
-</html>
+    setcookie("my_name_is","Shashwat", time() + (86400 * 2), "/");
+    setcookie("name_is","Yash", time() + (86400 * 3), "/");
+    setcookie("is_name","Aniket", time() + (86400 * 4), "/");
 
-<?php
-    include("footer.html") ;
+    if(isset($_COOKIE["my_name_is"])){
+        echo "Your name is {$_COOKIE["my_name_is"]}!!!";
+    } else {
+        echo "Cookie named 'my_name_is' is not set!";
+    };
 ?>
