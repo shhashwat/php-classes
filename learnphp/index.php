ad39476dca9ba303c346fe0a0ded361dabd73f66
Shashwat Singh <sshashwatssingh@gmail.com>
2025-03-31 14:24:34 +0530

    sql connected

--- learnphp/index.php
+++ learnphp/index.php
@@ -1,10 +1,15 @@
 <?php
-    $password = "hvy";
-    $hash = password_hash($password, PASSWORD_DEFAULT);
-
-    if(password_verify("hvy", $hash)){
-        echo "Password is valid!";
-    }else{
-        echo "Invalid Password";
-    };
+    include("database.php");
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home</title>
+</head>
+<body>
+    Hello <br>
+</body>
+</html>
