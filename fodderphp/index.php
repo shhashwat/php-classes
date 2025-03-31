f6c2df8f2c8d3a147859a769c5e6aea8acef9ad5
Shashwat Singh <sshashwatssingh@gmail.com>
2025-03-31 11:28:43 +0530

    yes

---
+++ fodderphp/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="includes/signup.inc.php" method="post">
+        <input type="text" name="username">
+        <input type="password" name="pwd">
+        <button>Sign-up</button>
+        <p>Or dont, not my problem</p>
+    </form>
+    
+    <?php
+    ?>
+</body>
+</html>
--- learnphp/index.php
+++ learnphp/index.php
@@ -1,33 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <form action="index.php" method="post">
-        username: <br>
-        <input type="text" name="username"><br>
-        age: <br>
-        <input type="age" name="age"><br>
-        email: <br>
-        <input type="email" name="email"><br>
-        <input type="submit" value="Login" name="login">
-    </form>
-</body>
-</html>
+<?php
 
-<?php 
-    if(isset($_POST["login"])){
-        $age = filter_input(INPUT_POST, "age", FILTER_VALIDATE_INT);
-        $email = filter_input(INPUT_POST,"email", FILTER_VALIDATE_EMAIL);
-        // $username = filter_input(INPUT_POST,"username", FILTER_VALIDATE_);
-
-        if(empty($email)){
-            echo "ENTER A VALID email";
-        } else{
-            echo "Your email: $email";
-        }
-    }
 ?>
